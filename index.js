a20ed23e0254b9f4a5750850bd33312e60b78c45
Randy <randyisbusy@gmail.com>
2020-02-28 20:33:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+
+let num1 = 62;
+let num2 = 1;  
+
+const multiply = num1 * num2
+ 
+const random = Math.random();
+ 
+const mod = 15 % 11 
+
+
+
+const max = Math.max( 1,2,3,20);
+ 
